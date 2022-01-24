1b15d8784c7cca6c2d26f5fe99d159777c80866c
sh1okoh <kooooo.0719task@gmail.com>
2022-01-24 15:25:49 +0900

    execute migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_045500) do
+ActiveRecord::Schema.define(version: 2022_01_24_062422) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
